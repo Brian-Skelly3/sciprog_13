90da93bd1e196520796dbf3f24f9633072dae950
Brian-Skelly3 <brian.skelly@ucdconnect.ie>
2022-11-21 23:28:01 +0000

    Upload of practical 10

---
+++ practical10/Hanoi.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void hanoi(int n, int source, int destination, int inter);
+
+
+int main(void){
+   
+   int h;
+   printf("Please pick a number of discs\n");
+   scanf("%d", &h);
+   if (h<1){
+      printf("h must be  apositive integer\n");
+      return 1;
+   }
+
+   printf("Solution for %d discs is\n", h);
+   hanoi(h, 1, 3, 2);
+      
+   return 0;
+}
+
+
+
+void hanoi(int n, int source, int destination, int inter){
+   if (n ==1){
+      printf("Move the disc from %d to %d\n", source, destination);
+   }
+   else{
+      hanoi(n-1, source, inter, destination);
+      printf("Move the disc from %d to %d\n", source, destination);
+      hanoi(n-1, inter, destination, source);
+   }
+}
+
+
+
---
+++ practical10/README.md
@@ -0,0 +1,1 @@
+This problem gives the solution for the Tower of Hanoi problem. Pick the number of discs you have, and it prints the steps of the solution.
