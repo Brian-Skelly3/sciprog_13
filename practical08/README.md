658831fc16752b0d1d79edeb7c64b7a8a77060db
Brian-Skelly3 <brian.skelly@ucdconnect.ie>
2022-11-21 20:26:44 +0000

    created practical08

---
+++ practical08/README.md
@@ -0,0 +1,1 @@
+gcd.c uses two methods based on Euclid's algorithm to find the greatest common divisor of two numbers. It first uses an iterative method, and uses a recursiev method.
---
+++ practical08/gcd.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+int itergcd(int a, int b);
+int recurgcd(int a, int b);
+
+int main(void){
+
+   int a, b, ierror;
+
+   printf("Please input two positive integers:\n");
+   ierror = scanf("%d %d", &a, &b);
+   if(ierror !=2){
+      printf("Please enter two positive integers\n");
+      return 1;
+   }
+
+   if (a<=0 || b<=0){
+      printf("These numbers are not positive\n");
+      return 1;
+}
+
+   //Call the functions
+   printf("Iterative gcd(%d, %d = %d)\n", a, b, itergcd(a, b));
+   printf("Recursive gcd(%d, %d = %d)\n", a, b, recurgcd(a, b));
+
+   return 0;
+}
+
+
+
+int itergcd(int a, int b){
+   int temp;
+   while(b != 0){
+      temp = b;
+      b = a%b;
+      a = temp;
+   }
+   return a;
+}
+
+
+int recurgcd(int a, int b){
+   if (b == 0){
+      return a;
+   }
+   else{
+      return (recurgcd(b, a%b));
+   }
+}
+
