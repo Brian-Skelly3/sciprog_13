e5cf86560db929100b1db0e857cece355df8dabf
Brian-Skelly3 <114590757+Brian-Skelly3@users.noreply.github.com>
2022-11-22 00:51:06 +0000

    Update Hanoi.c
    Added comments

--- practical10/Hanoi.c
+++ practical10/Hanoi.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 void hanoi(int n, int source, int destination, int inter);
-
 
 int main(void){
    
@@ -16,11 +14,10 @@ int main(void){
    }
 
    printf("Solution for %d discs is\n", h);
-   hanoi(h, 1, 3, 2);
+   hanoi(h, 1, 3, 2);   //Start on the first line, and end on the last line, and the other value is the middle rod.
       
    return 0;
 }
-
 
 
 void hanoi(int n, int source, int destination, int inter){
@@ -30,9 +27,6 @@ void hanoi(int n, int source, int destination, int inter){
    else{
       hanoi(n-1, source, inter, destination);
       printf("Move the disc from %d to %d\n", source, destination);
-      hanoi(n-1, inter, destination, source);
+      hanoi(n-1, inter, destination, source);    //Using recursion to cahnge where the start and end points are
    }
 }
-
-
-
