f807235b1658e2b2ae0364f2ced7853ea84f425c
Brian-Skelly3 <114590757+Brian-Skelly3@users.noreply.github.com>
2022-11-22 00:30:57 +0000

    Update matmult.c
    Tidied the code up and added comments

--- practical06/matmult.c
+++ practical06/matmult.c
@@ -8,6 +8,7 @@ int main(void){
    int n=5, p=3, q=4;
    double A[n][p], B[p][q], C[n][q];
 
+   //Creating the matrix A. This method will be repeated for matrix B.
    printf("This is the matrix A\n");
    for (i=0; i<n; i++){
       for (j=0; j<p; j++){
@@ -16,7 +17,6 @@ int main(void){
       }
       printf("\n");
    }
-
 
    printf("This is matrix B\n");
    for (i=0; i<p; i++){
@@ -30,13 +30,10 @@ int main(void){
    //print out the matrices
    
    printf("This is the matrix C\n");
-   matmult(A, B);
-
+   matmult(A, B);   //Create matrix C using the subroutine.
 
    return 0;
 }
-
-
 
 
 double matmult(double A[5][3], double B[3][4]){
@@ -45,6 +42,7 @@ double matmult(double A[5][3], double B[3][4]){
    int n=5, p=3, q=4;
    double C[n][q];
 
+   //Make C the right size and ititialise every value as 0
    for (i=0; i<n; i++){
       for (j=0; j<q; j++){
          C[i][j] = 0.0;
@@ -54,20 +52,10 @@ double matmult(double A[5][3], double B[3][4]){
    for (h=0; h<n; h++){
       for (j=0; j<q; j++){
          for (i=0; i<p; i++){
-
-            C[h][j] += A[h][i] * B[i][j];    
+            C[h][j] += A[h][i] * B[i][j];    //Multiplied the right sections of A and B to create C
          }
-      
-         printf("%3.0lf", C[h][j]);
-
+         printf("%3.0lf", C[h][j]);   //Print C here rather than in main as the for loops are already here.
       }
       printf("\n");
-    }
-
+   }
 }
-
-
-
-
-
-
